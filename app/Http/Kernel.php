a11793b838ee1fda6c493be0dacb51bc26c3ef3d
saeedalaloul <saeedalaloul@gmail.com>
2021-10-31 09:09:28 +0200

    make it better

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \App\Http\Middleware\ForceHttpsMiddleware::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
@@ -38,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\HttpsProtocolMiddleware::class
         ],
 
         'api' => [
--- app/Http/Middleware/HttpsProtocolMiddleware.php
+++ app/Http/Middleware/HttpsProtocolMiddleware.php
@@ -5,12 +5,18 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\App;
 
-class ForceHttpsMiddleware
+class HttpsProtocolMiddleware
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
     public function handle($request, Closure $next)
     {
-
-        if (!$request->secure() && App::environment() === 'production') {//optionally disable for localhost development
+        if (!$request->secure() && app()->environment('production')) {
             return redirect()->secure($request->getRequestUri());
         }
 
