29d97f5fff636239f4f7becfcf928a4e3a46c48c
saeedalaloul <saeedalaloul@gmail.com>
2021-10-31 09:35:16 +0200

    make it better

--- app/Http/Middleware/ForceHttpsMiddleWare.php
+++ app/Http/Middleware/ForceHttpsMiddleWare.php
@@ -3,20 +3,12 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 
 class ForceHttpsMiddleWare
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && app()->environment('production')) {
+        if (!$request->secure()) {
             return redirect()->secure($request->getRequestUri());
         }
 
