6839c6c44b4e05d251261ced284173c37c16ff37
saeedalaloul <saeedalaloul@gmail.com>
2021-10-31 09:20:05 +0200

    make it better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-//        if (env('APP_ENV') === 'production') {
-//            \Illuminate\Support\Facades\URL::forceScheme('https');
-//        }
+        if (env('APP_ENV') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -48,7 +48,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
         });
 
-        //resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
     }
 
     /**
