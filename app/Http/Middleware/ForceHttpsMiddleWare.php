e69987503fe28c08ac2eb4f1fc3c039e4407f4bd
saeedalaloul <saeedalaloul@gmail.com>
2021-11-02 14:57:54 +0200

    make it better

--- app/Http/Middleware/ForceHttpsMiddleWare.php
+++ app/Http/Middleware/ForceHttpsMiddleWare.php
@@ -3,13 +3,17 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class ForceHttpsMiddleWare
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure()) {
-            return redirect()->secure($request->getRequestUri());
+        // check if environment is production
+        if(env('APP_ENV') === "production") {
+            if (!$request->secure()) {
+                return redirect()->secure($request->path());
+            }
         }
 
         return $next($request);
