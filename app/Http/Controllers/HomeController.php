6da1fa2142ebe0d184d566482928a2355a18a8ef
saeedalaloul <saeedalaloul@gmail.com>
2021-11-02 17:40:31 +0200

    make it better

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,31 +27,31 @@ class HomeController extends Controller
         $this->checkRoles();
 
 
-        $fields = array(
-            'app_id' => "84b0c55f-c845-4f78-be68-f9bea972f208",
-            'identifier' => "ce777617da7f548fe7a9ab6febb56cf39fba6d382000c0395666288d961ee566",
-            'language' => "en",
-            'country' => "ps",
-            'notification_types' => 1,
-        );
-
-        $fields = json_encode($fields);
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/players");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        curl_setopt($ch, CURLOPT_POST, TRUE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        $return["allresponses"] = $response;
-        $return = json_encode( $return);
-
+//        $fields = array(
+//            'app_id' => "84b0c55f-c845-4f78-be68-f9bea972f208",
+//            'identifier' => "ce777617da7f548fe7a9ab6febb56cf39fba6d382000c0395666288d961ee566",
+//            'language' => "en",
+//            'country' => "ps",
+//            'notification_types' => 1,
+//        );
+//
+//        $fields = json_encode($fields);
+//
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/players");
+//        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+//        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+//        curl_setopt($ch, CURLOPT_POST, TRUE);
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+//
+//        $response = curl_exec($ch);
+//        curl_close($ch);
+//
+//        $return["allresponses"] = $response;
+//        $return = json_encode( $return);
+//
 
         return view('dashboard');
     }
