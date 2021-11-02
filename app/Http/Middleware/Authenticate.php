dcac089fdd0f93c09b67354e2f09f795cc1698c2
saeedalaloul <saeedalaloul@gmail.com>
2021-11-02 15:48:03 +0200

    make it better

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,6 +14,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        dd('this');
         if (!$request->expectsJson()) {
             return route('login');
         }
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,6 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        dd('this');
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
