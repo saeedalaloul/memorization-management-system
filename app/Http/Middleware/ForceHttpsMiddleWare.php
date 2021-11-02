7ae7bb341a5a97d475286c6e9f0d498a449e5696
saeedalaloul <saeedalaloul@gmail.com>
2021-11-02 16:12:59 +0200

    make it better

--- app/Http/Middleware/ForceHttpsMiddleWare.php
+++ app/Http/Middleware/ForceHttpsMiddleWare.php
@@ -11,7 +11,7 @@ class ForceHttpsMiddleWare
     public function handle(Request $request, Closure $next)
     {
         if (!$request->secure() && App::environment(['production'])) {
-            return redirect()->secure($request->getRequestUri());
+            return redirect()->secure($request->getRequestUri(), 301);
         }
 
         return $next($request);
