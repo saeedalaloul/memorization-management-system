7951aa9519aebbc30e5b598aab8a79de55e32833
saeedalaloul <saeedalaloul@gmail.com>
2021-11-03 21:09:43 +0200

    make it better

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -33,7 +33,6 @@
         OneSignal.init({
             appId: "c99992cc-e40f-46d1-8f7c-a5e4efd99c88",
             autoResubscribe: true,
-            autoRegister: false,
             notifyButton: {
                 enable: true,
             },
