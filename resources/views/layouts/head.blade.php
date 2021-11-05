a7e8a278f231c3712a25819c333ee7db34a09c68
saeedalaloul <saeedalaloul@gmail.com>
2021-11-05 14:42:57 +0200

    make it better

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -35,8 +35,8 @@ if (auth()->id() != null && \App\Models\UserSubscribeNotification::where('id', a
     OneSignal.push(function () {
         OneSignal.init({
             appId: "c99992cc-e40f-46d1-8f7c-a5e4efd99c88",
-            external_user_id: {{strval(auth()->id())}},
-            player_id: {{strval($player_id)}},
+            external_user_id: "{{auth()->id()}}",
+            player_id: "{{$player_id}}",
             autoResubscribe: true,
             notifyButton: {
                 enable: true,
